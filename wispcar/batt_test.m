1e2f8fce72c8f6720b7eb8d2ee5e9285063088e6
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2009-06-25 23:12:17 +0000

    wispcarb sch

--- wispcar/batt_test.m
+++ wispcar/batt_test.m
@@ -2,13 +2,33 @@
 % David Rowe 23 June 2009
 % Plot battery test results
 
-function batt_test(f)
+function batt_test(f,pngname)
   d = load(f);
   mins = d(:,1)*60 + d(:,2);
   mins = mins - mins(1);
-  plot(mins, 0.072*d(:,4) + 0.82,'*');
+  s = sprintf(';%s;',f);
+  plot(mins, 0.072*d(:,4) + 0.82,s);
   xlabel('time (min)');  ylabel('V');
   axis([0 60 11 13]) 
   grid
+
+  if (nargin == 2)
+
+    % small image
+
+    __gnuplot_set__ terminal png size 420,300
+    s = sprintf("__gnuplot_set__ output \"%s.png\"", pngname);
+    eval(s)
+    replot;
+
+    % larger image
+
+    __gnuplot_set__ terminal png size 800,600
+    s = sprintf("__gnuplot_set__ output \"%s_large.png\"", pngname);
+    eval(s)
+    replot;
+
+  endif
+
 endfunction
 
