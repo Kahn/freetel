685136359a0a378c11d75b9fc9c084636290b0fb
drowe67 <david@rowetel.com>
2010-01-04 06:09:13 +0000

    first pass session support, needs to be improved

---
+++ easy-asterisk-gui/logout.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# logout.sh
+# David Rowe 4 Jan 2010
+# CGI for Easy Asterisk logout GUI
+
+cat <<EOF
+Content-type: text/html
+Set-Cookie: loggedin=1; expires=Thursday, 01-Jan-98 12:00:00 GMT
+
+<title>Easy Asterisk - Logout</title>
+EOF
+echo $HTTP_COOKIE
