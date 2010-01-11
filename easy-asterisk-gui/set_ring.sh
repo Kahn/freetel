e537318cedc11c0af06e14c19bb8cfbdec878874
drowe67 <david@rowetel.com>
2010-01-11 22:37:01 +0000

    tested incoming calls with 1,2,3 extensions

--- easy-asterisk-gui/set_ring.sh
+++ easy-asterisk-gui/set_ring.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 # set_ring.sh
 # David Rowe 4 Jan 2010
 # CGI to set which extensions ring on uncoming calls
@@ -12,14 +12,21 @@ if [ $? -eq 1 ]; then
     echo '<meta http-equiv="REFRESH" content="0;url=http:login.sh">'
     echo "</head>"
     echo "</html>"
-    exit
+#    exit
 fi
 
 # extract extensions to ring and reload extensions.conf
 
-args=`echo "$QUERY_STRING" | sed -n "s/=on*//pg" | sed -n 's,_,/,pg' | sed -n "s/&/\\\\&/p"`
-sed -i "s_s,1,Dial(.*) ;; easy-asterisk_s,1,Dial($args) ;; easy-asterisk_" /etc/asterisk/extensions.conf
-asterisk -rx "dialplan reload" >> /dev/null
+a=`echo "$QUERY_STRING" | sed -n "s/=on*//pg" | sed -n 's,_,/,pg'`
+
+# escape & if present
+echo "$a" | grep "&" >> /dev/null
+if [ $? -eq 0 ]; then
+  a=`echo $a | sed -n "s/&/\\\\&/pg"`
+fi
+
+sed -i "s_s,1,Dial(.*) ;; easy-asterisk_s,1,Dial($a) ;; easy-asterisk_" /etc/asterisk/extensions.conf
+asterisk -rx "dialplan reload" 2>/dev/null 1 > /dev/null
 
 # bounce us back to Phones screen
 
@@ -30,6 +37,13 @@ cat <<EOF
 <meta http-equiv="REFRESH" content="0;url=http:phones.sh">
 <body>
 Please wait a few seconds.....
+EOF
+#echo $QUERY_STRING "<br>"
+#echo "$QUERY_STRING" | sed -n "s/=on*//pg" | sed -n 's,_,/,pg'
+#echo "<br>"
+#echo $a
+
+cat <<EOF
 </body>
 </head>
 </html>
