ba500bd78d429f9fb1baa1094ef0af175c18a55b
drowe67 <david@rowetel.com>
2010-01-16 04:20:30 +0000

    removed some executable properties

--- easy-asterisk-gui/cgi-bin/check_loggedin.sh
+++
@@ -1,9 +0,0 @@
-echo $HTTP_COOKIE | grep "loggedin" > /dev/null
-if [ $? -eq 1 ]; then
-    echo "<html>"
-    echo "<head>"
-    echo '<meta http-equiv="REFRESH" content="0;url=http:login.sh">'
-    echo "</head>"
-    echo "</html>"
-    exit
-fi
