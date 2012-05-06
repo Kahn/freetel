7420ec36c4e44ba4ea887494101f01d155f15bbd
horse69 <horse69@01035d8c-6547-0410-b346-abe4f91aad63>
2012-05-06 22:59:01 +0000

    comments

--- mini-asterisk-gui2/about.js
+++ mini-asterisk-gui2/about.js
@@ -1,9 +1,9 @@
 /*
   about.js
   Ian Roberts
-  May 7 2010
+  May 7 2012
 
-  About screen for Mini Asterisk GUI.
+  About screen for Mini Asterisk GUI2.
 */
 
 var update_time = 10;
@@ -31,6 +31,7 @@ function initialisePage() {
     html += '<tr><td colspan="3" align="left" valign="top"><h2>About</h2></td></tr>';
     html += '<tr><td colspan="2"><a href="http://rowetel.com/ucasterisk/mini.html">Mini Asterisk GUI2</a></td><td> </td><td>Revision XXX</td></tr>';
     html += '<tr><td colspan="2">Brought to you by the</td><td> </td><td> <a href="http://rowetel.com/ucasterisk/index.html">Free Telephony Project</a></td></tr>';
+    // this image will only come up if internet connection is present
     html += '<tr><td colspan="2"><a href="http://rowetel.com/ucasterisk/ip04.html"><img src="http://rowetel.com/images/ip04/ip04_case.jpg" border="0" /></a></td><td> </td><td></td></tr>';
     html += '<tr><td>&nbsp</td></tr>';
  
