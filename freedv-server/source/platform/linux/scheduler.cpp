709d45de0f54329731b930b9a2b08c29328eb363
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-01-22 01:11:57 +0000

    Add all of the necessary methods to codec and modem. The program won't compile until I fill them in.

--- freedv-server/source/platform/linux/scheduler.cpp
+++ freedv-server/source/platform/linux/scheduler.cpp
@@ -7,6 +7,21 @@
 #endif
 #include "drivers.h"
 
+static const char insufficient_privilege_message[] =
+"Warning: Insufficient privilege to set real-time priority.\n"
+"This could cause audio to be interrupted as other programs run.\n" 
+"To fix: As root, run\n"
+"\n"
+"\tsetcap cap_sys_nice+ep filename\n"
+"\n"
+"where filename is the executable file for this program. That will\n"
+"allow you to use real-time priority while running as any user.\n"
+"Alternatively, you can execute this program as root.\n\n";
+
+static const char old_kernel_message[] =
+"This kernel doesn't seem to have real-time facilities.\n"
+"If audio is sometimes interrupted, try a newer kernel.\n\n";
+
 namespace FreeDV {
   void
   set_scheduler()
@@ -18,14 +33,10 @@ namespace FreeDV {
     p.sched_priority = sched_get_priority_min(policy);
     const int result = sched_setscheduler(0, policy, &p);
     if ( result < 0 ) {
-      if ( errno == EPERM ) {
-        std::cerr << "Insufficient privilege to set real-time priority. "
-	 "Try runing as root." << std::endl;
-      }
-      else if ( errno == EINVAL ) {
-        std::cerr << "This kernel doesn't seem to have real-time facilities. "
-         "If audio is sometimes interrupted, try a newer kernel." << std::endl;
-      }
+      if ( errno == EPERM )
+         std::cerr << insufficient_privilege_message << std::flush;
+      else if ( errno == EINVAL )
+         std::cerr << old_kernel_message << std::flush;
       else {
         std::cerr << "sched_setscheduler: " << strerror(errno) << std::endl;
       }
