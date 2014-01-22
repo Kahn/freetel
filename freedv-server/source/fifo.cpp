01302de356afd07508ff9199a40ea761d8e38823
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-01-22 05:33:43 +0000

    FIFO implemented but not tested.

--- freedv-server/source/fifo.cpp
+++ freedv-server/source/fifo.cpp
@@ -1,14 +1,39 @@
 #include "drivers.h"
+#include <stdexcept>
+#include <cstring>
 
 namespace FreeDV {
+  FIFO::FIFO(std::size_t size)
+  : buffer(new char[size]), buffer_end(buffer + size)
+  {
+    
+  }
+
   char *
   FIFO::reorder(std::size_t size)
   {
-    return 0;
+    const std::size_t bytes = in - out;
+
+    if ( bytes > 0 ) {
+
+      // memmove() is specified to handle overlap properly.
+      memmove(buffer, out, bytes);
+#ifdef DEBUG
+      std::cerr << "FIFO reorder with copy." << std::endl;
+#endif
+    }
+    out = buffer;
+    in = buffer + bytes;
+
+    if ( size > buffer_end - in )
+      throw std::runtime_error("FIFO incoming data overrun.");
+
+    return in;
   }
 
   void
   FIFO::out_overrun(std::size_t size) const
   {
+    throw std::runtime_error("FIFO outgoing data overrun.");
   }
 }
