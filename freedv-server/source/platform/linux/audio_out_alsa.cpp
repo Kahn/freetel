54d1716aa3612ee2d96a8fc2435689fdeeb5f8d6
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-03-11 03:40:18 +0000

    Fill in audio_out_alsa

--- freedv-server/source/platform/linux/audio_out_alsa.cpp
+++ freedv-server/source/platform/linux/audio_out_alsa.cpp
@@ -1,5 +1,6 @@
 /// The ALSA audio output driver. 
 
+#include <stdlib.h>
 #include "drivers.h"
 #include <alsa/asoundlib.h>
 #include <sys/ioctl.h>
@@ -12,8 +13,20 @@ namespace FreeDV {
   /// Audio output "ALSA", Uses the Linux ALSA Audio API.
   class AudioOutALSA : public AudioOutput {
   private:
-    snd_pcm_t * handle;
+    char * const	parameters;
+    snd_pcm_t *		handle;
 
+    volatile void
+    do_throw(const int error, const char * message = 0)
+    {
+      std::ostringstream str;
+
+      str << "Error on ALSA audio output " << parameters << ": ";
+       if ( message )
+         str << message << ": ";
+       str << snd_strerror(error);
+      throw std::runtime_error(str.str().c_str());
+    }
   public:
 
 	/// Instantiate the audio output.
@@ -31,32 +44,56 @@ namespace FreeDV {
   };
 
   AudioOutALSA::AudioOutALSA(const char * p)
-  : AudioOutput("alsa", p)
+  : AudioOutput("alsa", p), parameters(strdup(p))
   {
-    const int error = snd_pcm_open(
+    const snd_pcm_format_t  	format = SND_PCM_FORMAT_S16;
+    const snd_pcm_access_t  	access = SND_PCM_ACCESS_RW_INTERLEAVED;
+    const unsigned int  	channels = 1;
+    const unsigned int  	rate = 48000;
+    const int			soft_resample = 0;
+    const unsigned int  	latency = 0;
+    int				error;
+
+    handle = 0;
+    error = snd_pcm_open(
      &handle,
      p,
      SND_PCM_STREAM_PLAYBACK,
      0);
 
-    if ( error ) {
-      std::ostringstream str;
+    if ( error != 0 )
+      do_throw(error, "Open");
 
-      str << "Can't open audio device " << p << ": " << snd_strerror(error);
-      throw std::runtime_error(str.str().c_str());
-    }
+    if ( (error = snd_pcm_set_params(
+     handle,
+     format,
+     access,
+     channels,
+     rate,
+     soft_resample,
+     latency)) < 0 )
+      do_throw(error, "Set Parameters");
+
+    if ( (error = snd_pcm_prepare(handle)) < 0 )
+      do_throw(error, "Prepare");
   }
 
   AudioOutALSA::~AudioOutALSA()
   {
+    snd_pcm_drop(handle);
     snd_pcm_close(handle);
+    free(parameters);
   }
 
   // Write audio into the "short" type.
   std::size_t
   AudioOutALSA::write16(const std::int16_t * array, std::size_t length)
   {
-    return length;
+    const int result = snd_pcm_writei(handle, array, length);
+    if ( result >= 0 )
+      return result;
+    else
+      do_throw(result, "Write");
   }
 
   AudioOutput *
@@ -76,10 +113,10 @@ namespace FreeDV {
   {
     snd_pcm_sframes_t	available = 0;
 
-    if ( (available = snd_pcm_avail(handle)) <= 0 )
+    if ( (available = snd_pcm_avail(handle)) >= 0 )
       return available;
     else
-      return 0;
+      do_throw(available, "Get Available Frames");
   }
 
   static bool
