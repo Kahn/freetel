5fa418c673ee23839c660165999a7d4ef6b27959
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-03-13 20:59:39 +0000

    More tuning of periods. Add mloclkall() so that paging won't interrupt real-time performance. Add message about the need to set setcap cap_ipc_lock+ep

--- freedv-server/source/platform/linux/scheduler.cpp
+++ freedv-server/source/platform/linux/scheduler.cpp
@@ -1,46 +1,71 @@
 #include <unistd.h>
 #ifdef _POSIX_PRIORITY_SCHEDULING
-#include <errno.h>
 #include <sched.h>
+#endif
+#ifdef _POSIX_MEMLOCK_RANGE
+#include <sys/mman.h>
+#endif
+#include <errno.h>
 #include <cstring>
 #include <iostream>
-#endif
 #include "drivers.h"
 
 static const char insufficient_privilege_message[] =
-"Warning: Insufficient privilege to set a real-time scheduling priority.\n"
+"Warning: Insufficient privilege to set a real-time scheduling priority,\n"
+"or to lock memory.\n"
 "This could cause audio to be interrupted while other programs use the CPU.\n" 
 "To fix: As root, run\n"
 "\n"
 "\tsetcap cap_sys_nice+ep filename\n"
+"\tsetcap cap_ipc_lock+ep filename\n"
 "\n"
-"where filename is the executable file for this program. That will\n"
-"allow you to use a real-time scheduling priority while running as any user.\n"
-"Alternatively, you can execute this program as root.\n\n";
+"where filename is the executable file for this program.\n"
+"That will allow you to use a real-time scheduling priority and locked memory\n"
+"while running as any user.\n"
+"Alternatively, you can execute this program as root.\n";
 
 static const char old_kernel_message[] =
-"This kernel doesn't seem to have real-time facilities.\n"
-"If audio is sometimes interrupted, try a newer kernel.\n\n";
+"This kernel doesn't seem to have real-time facilities or memory locking.\n"
+"If audio is sometimes interrupted, try a newer kernel.\n";
 
 namespace FreeDV {
   void
   set_scheduler()
   {
+    bool insufficient_privilege = false;
+    bool old_kernel = false;
+
 #ifdef _POSIX_PRIORITY_SCHEDULING
+    // Put this process on the round-robin realtime scheduling queue at the
+    // minimum priority. Other real-time processes (perhaps portaudio) can
+    // run with a higher priority than this, but this process will run at
+    // a higher priority than all normal processes.
     sched_param	p;
     int		policy = SCHED_RR;
 
     p.sched_priority = sched_get_priority_min(policy);
     const int result = sched_setscheduler(0, policy, &p);
     if ( result < 0 ) {
+      std::cerr << "sched_setscheduler: " << strerror(errno) << std::endl;
+      if ( errno == EINVAL )
+         old_kernel = true;
       if ( errno == EPERM )
-         std::cerr << insufficient_privilege_message << std::flush;
-      else if ( errno == EINVAL )
-         std::cerr << old_kernel_message << std::flush;
-      else {
-        std::cerr << "sched_setscheduler: " << strerror(errno) << std::endl;
-      }
+         insufficient_privilege = true;
     }
-  }
 #endif
+
+#ifdef _POSIX_MEMLOCK_RANGE
+    if ( mlockall(MCL_CURRENT|MCL_FUTURE) < 0 ) {
+      std::cerr << "mlockall: " << strerror(errno) << std::endl;
+      if ( errno == EINVAL )
+         old_kernel = true;
+      if ( errno == EPERM )
+         insufficient_privilege = true;
+    }
+#endif
+    if ( old_kernel )
+      std::cerr << old_kernel_message;
+    else if ( insufficient_privilege )
+      std::cerr << insufficient_privilege_message;
+  }
 }
