8b91c3b849a82a43e350f1720993ec5202dfd1d4
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-04-18 04:14:12 +0000

    Complete enumeration of evdev.

--- freedv-server/source/platform/linux/evdev.h
+++ freedv-server/source/platform/linux/evdev.h
@@ -9,20 +9,30 @@
 #define test_bit(bit, array)	((array[LONG(bit)] >> OFF(bit)) & 1)
 
 class EvDev {
+protected:
+  struct device_enumeration {
+    const char *	special_file;
+    const char *	name;
+    uint8_t		event_types[(EV_MAX + 7) / 8];
+  };
+
 private:
 
 			EvDev(const EvDev &);
   EvDev &		operator =(const EvDev &);
 
-protected:
-  struct device_enumeration {
-    const char *	device;
-    const char *	name;
-    uint8_t		event_types[(EV_MAX + 7) / 8];
-  };
+  static bool		enumerate_device(
+			 const char *			name,
+			 int				fd,
+			 struct device_enumeration &	device);
 
-  static const device_enumeration *
-			enumerate(std::size_t & size);
+protected:
+  static void		delete_enumeration(
+ 			 device_enumeration *	data,
+			 std::size_t		count);
+
+  static device_enumeration *
+			enumerate(std::size_t & count);
 
 public:
   static const char * const *
