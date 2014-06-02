ca74d9717b97bf308e93edfc54fd27a9320ff6cf
bruceperens <bruceperens@01035d8c-6547-0410-b346-abe4f91aad63>
2014-06-02 07:21:07 +0000

    Moving FreeDV-Server to Algoram GIT server.

--- freedv-server/source/global.cpp
+++
@@ -1,9 +0,0 @@
-#include "drivers.h"
-
-namespace FreeDV {
-  /// Global name of the program, for error messages.
-  const char * program_name = 0;
-
-  /// Global driver manager object.
-  DriverManager * _globalDriverManager = 0;
-}
--- freedv-server/source/tiny_main.cpp
+++
@@ -1,5 +0,0 @@
-/// \file tiny_main.cpp
-/// The tiny main program for embedded use.
-///
-/// \copyright Copyright (C) 2013-2014 Algoram. See the LICENSE file.
-///
