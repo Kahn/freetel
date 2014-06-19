01859d3c1c03218f32e3d6f4a165a80dc10b403e
drowe67 <david@rowetel.com>
2014-06-19 02:16:01 +0000

    removed some more unneeded files and directories

--- fdmdv2-dev/DEBIAN_DEVEL.sh
+++
@@ -1,1 +0,0 @@
-apt-get install cmake gcc g++ libportaudio-dev libhamlib-dev libsamplerate-dev libsndfile-dev libsox-dev libgtk-3-dev libasound-dev
--- fdmdv2-dev/DEBIAN_RUNTIME.sh
+++
@@ -1,1 +0,0 @@
-apt-get install libportaudio2 libhamlib2 libsamplerate0 libsndfile1 libsox2 libgtk-3-0 libasound2
