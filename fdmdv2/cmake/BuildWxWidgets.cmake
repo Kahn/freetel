50d5d8518f8b69abcc7089e132df6005622dbc15
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2014-09-28 20:02:20 +0000

    Update cmake config and fix Windows NSIS packager to find depdendencies automatically.

--- fdmdv2/cmake/BuildWxWidgets.cmake
+++ fdmdv2/cmake/BuildWxWidgets.cmake
@@ -5,7 +5,11 @@ ExternalProject_Add(wxWidgets
     URL http://downloads.sourceforge.net/wxwindows/${WXWIDGETS_TARBALL}.tar.bz2
     BUILD_IN_SOURCE 1
     INSTALL_DIR external/dist
-    CONFIGURE_COMMAND ./configure --disable-shared --prefix=${CMAKE_BINARY_DIR}/external/dist
+    if(MINGW)
+        CONFIGURE_COMMAND mingw32-make -f ${source_dir}/build/msw/makefile.gcc SHARED=0 UNICODE=1 BUILD=release PREFIX=${CMAKE_BINARY_DIR}/external/dist
+    else(MINGW)
+        CONFIGURE_COMMAND ./configure --disable-shared --prefix=${CMAKE_BINARY_DIR}/external/dist
+    endif(MINGW)
     BUILD_COMMAND $(MAKE)
     INSTALL_COMMAND $(MAKE) install
 )
@@ -13,5 +17,6 @@ ExternalProject_Add(wxWidgets
 ExternalProject_Get_Property(wxWidgets install_dir)
 set(WXCONFIG "${install_dir}/bin/wx-config")
 set(WXRC "${install_dir}/bin/wxrc")
-#list(APPEND FREEDV_LINK_LIBS ${wxWidgets_LIBRARIES})
-#list(APPEND FREEDV_STATIC_DEPS wxWidgets)
+if(EXISTS ${WXCONFIG})
+    set(BS_WX_DONE TRUE)
+endif()
