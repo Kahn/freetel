eb59f018b98b77542933994a09fe2bb991a1fdd6
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2014-12-12 14:02:44 +0000

    Add missing hamlib rig libraries and other tweakes.

--- fdmdv2/cmake/BuildSndfile.cmake
+++ fdmdv2/cmake/BuildSndfile.cmake
@@ -5,12 +5,12 @@ ExternalProject_Add(sndfile
     URL http://www.mega-nerd.com/libsndfile/files/${SNDFILE_TARBALL}.tar.gz
     BUILD_IN_SOURCE 1
     INSTALL_DIR external/dist
-    CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/external/dist --disable-external-libs
+    CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/external/dist --disable-external-libs --disable-shared
     BUILD_COMMAND $(MAKE)
     INSTALL_COMMAND $(MAKE) install
 )
 set(SNDFILE_LIBRARIES
-    ${CMAKE_BINARY_DIR}/external/dist/lib/libsndfile.a PARENT_SCOPE)
+    ${CMAKE_BINARY_DIR}/external/dist/lib/libsndfile.a)
 include_directories(${CMAKE_BINARY_DIR}/external/dist/include)
 list(APPEND FREEDV_LINK_LIBS ${SNDFILE_LIBRARIES})
 list(APPEND FREEDV_STATIC_DEPS sndfile)
