e144115efc3e39f3799d4c1ae5f831a23a481ace
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2015-03-03 16:03:57 +0000

    Fix static builds of sox and libsndfile.

--- fdmdv2/cmake/BuildSndfile.cmake
+++ fdmdv2/cmake/BuildSndfile.cmake
@@ -6,11 +6,15 @@ ExternalProject_Add(sndfile
     BUILD_IN_SOURCE 1
     INSTALL_DIR external/dist
     CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/external/dist --disable-external-libs --disable-shared
-    BUILD_COMMAND $(MAKE)
+    BUILD_COMMAND $(MAKE) V=1
     INSTALL_COMMAND $(MAKE) install
 )
-set(SNDFILE_LIBRARIES
-    ${CMAKE_BINARY_DIR}/external/dist/lib/libsndfile.a)
+if(MINGW)
+    set(SNDFILE_LIBRARIES ${CMAKE_BINARY_DIR}/external/dist/bin/libsndfile-1.dll)
+else()
+    set(SNDFILE_LIBRARIES ${CMAKE_BINARY_DIR}/external/dist/lib/libsndfile.a)
+endif()
+
 include_directories(${CMAKE_BINARY_DIR}/external/dist/include)
 list(APPEND FREEDV_LINK_LIBS ${SNDFILE_LIBRARIES})
 list(APPEND FREEDV_STATIC_DEPS sndfile)
