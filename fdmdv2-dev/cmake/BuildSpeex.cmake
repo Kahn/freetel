4131082cca08f42c1174bd95457d404378e13474
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2015-03-09 16:10:58 +0000

    Update dependencies and pass needed agruments to static Codec2 build since it also depends on Speex.

--- fdmdv2-dev/cmake/BuildSpeex.cmake
+++ fdmdv2-dev/cmake/BuildSpeex.cmake
@@ -1,5 +1,4 @@
 set(SPEEXDSP_TARBALL "speexdsp-1.2rc3.tar.gz")
-#set(SPEEXDSP_TARBALL "speex-1.0.5.tar.gz")
 
 include(ExternalProject)
 ExternalProject_Add(speex
@@ -15,3 +14,6 @@ set(SPEEXDSP_LIBRARIES ${CMAKE_BINARY_DIR}/external/dist/lib/libspeexdsp.a)
 include_directories(${CMAKE_BINARY_DIR}/external/dist/include)
 list(APPEND FREEDV_LINK_LIBS ${SPEEXDSP_LIBRARIES})
 list(APPEND FREEDV_STATIC_DEPS speex)
+if(USE_STATIC_CODEC2)
+    add_dependencies(codec2 speex)
+endif()
