0774f25ddca9ff7c418f9a559dd8a0ecc1a4493b
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2015-07-02 02:25:35 +0000

    Bump codec2-dev version to 0.4.0 and final tweaks for win32 cross-compiling.

--- fdmdv2/cmake/BuildCodec2.cmake
+++ fdmdv2/cmake/BuildCodec2.cmake
@@ -1,7 +1,11 @@
+if(CMAKE_CROSSCOMPILING)
+    set(CODEC2_CMAKE_ARGS "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
+endif()
+
 include(ExternalProject)
 ExternalProject_Add(codec2
    SVN_REPOSITORY https://svn.code.sf.net/p/freetel/code/codec2
-   CMAKE_ARGS -DBUILD_SHARED_LIBS=FALSE 
+   CMAKE_ARGS ${CODEC2_CMAKE_ARGS}
    INSTALL_COMMAND ""
 )
 set(CODEC2_LIBRARIES
