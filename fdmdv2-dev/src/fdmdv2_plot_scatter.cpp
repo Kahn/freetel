d365db1c05c2b8300fd1e373e9451640dc7fd334
drowe67 <david@rowetel.com>
2015-07-26 23:02:40 +0000

    checkin after sourceforge outage, improved scatter diagram, remembers mode, blinking on from radio file playback and warning of sample rate with raw files and handling Fs=8000Hz sample rate wave file on 700, freq scale of waterfall/spectrum in analog mode, portaudio framesperbuffer to prevent breakup on linux

--- fdmdv2-dev/src/fdmdv2_plot_scatter.cpp
+++ fdmdv2-dev/src/fdmdv2_plot_scatter.cpp
@@ -54,7 +54,7 @@ PlotScatter::PlotScatter(wxFrame* parent) : PlotPanel(parent)
         
 }
 
-// changing number of carriers changes number of symoles to plot
+// changing number of carriers changes number of symbols to plot
 void PlotScatter::setNc(int Nc) {
     Nsym = Nc+1;
     assert(Nsym <= (FDMDV_NC_MAX+1));
@@ -69,9 +69,29 @@ void PlotScatter::draw(wxAutoBufferedPaintDC& dc)
 {
     float x_scale;
     float y_scale;
-    int   i,j;
+    int   i;
     int   x;
     int   y;
+    wxColour sym_to_colour[] = {wxColor(0,0,255), 
+                                wxColor(0,255,0),
+                                wxColor(0,255,255),
+                                wxColor(255,0,0),
+                                wxColor(255,0,255), 
+                                wxColor(255,255,0),
+                                wxColor(255,255,255),
+                                wxColor(0,0,255), 
+                                wxColor(0,255,0),
+                                wxColor(0,255,255),
+                                wxColor(255,0,0),
+                                wxColor(255,0,255), 
+                                wxColor(255,255,0),
+                                wxColor(255,255,255),
+                                wxColor(0,0,255), 
+                                wxColor(0,255,0),
+                                wxColor(0,255,255),
+                                wxColor(255,0,0),
+                                wxColor(255,0,255)
+    };
 
     m_rCtrl = GetClientRect();
     m_rGrid = m_rCtrl;
@@ -87,9 +107,56 @@ void PlotScatter::draw(wxAutoBufferedPaintDC& dc)
     dc.DrawRectangle(m_rPlot);
  
     wxPen pen;
-    pen.SetColour(LIGHT_GREEN_COLOR);
-    pen.SetWidth(1);
-    dc.SetPen(pen);
+    pen.SetWidth(1); // note this is ignored by DrawPoint
+
+    // automatically scale, first measure the maximum value
+
+    float max_xy = 1E-12;
+    float real,imag;
+    for(i=0; i< scatterMemSyms; i++) {
+        real = fabs(m_mem[i].real);
+        imag = fabs(m_mem[i].imag);
+        if (real > max_xy)
+            max_xy = real;
+        if (imag > max_xy)
+            max_xy = imag;
+    }
+
+    // smooth it out and set a lower limit to prevent didev by 0 issues
+
+    m_filter_max_xy = BETA*m_filter_max_xy + (1 - BETA)*2.5*max_xy;
+    if (m_filter_max_xy < 0.001)
+        m_filter_max_xy = 0.001;
+
+    // quantise to log steps to prevent scatter scaling bobbing about too
+    // much as scaling varies
+
+    float quant_m_filter_max_xy = exp(floor(0.5+log(m_filter_max_xy)));
+    //printf("max_xy: %f m_filter_max_xy: %f quant_m_filter_max_xy: %f\n", max_xy, m_filter_max_xy, quant_m_filter_max_xy);
+
+    x_scale = (float)m_rGrid.GetWidth()/quant_m_filter_max_xy;
+    y_scale = (float)m_rGrid.GetHeight()/quant_m_filter_max_xy;
+
+    // draw all samples
+
+    for(i = 0; i < scatterMemSyms; i++)
+    {
+        x = x_scale * m_mem[i].real + m_rGrid.GetWidth()/2;
+        y = y_scale * m_mem[i].imag + m_rGrid.GetHeight()/2;
+	x += PLOT_BORDER + XLEFT_OFFSET;
+	y += PLOT_BORDER;
+        pen.SetColour(sym_to_colour[i%Nsym]);
+        dc.SetPen(pen);
+	dc.DrawPoint(x, y);
+    }
+}
+
+//----------------------------------------------------------------
+// add_new_samples()
+//----------------------------------------------------------------
+void PlotScatter::add_new_samples(COMP samples[])
+{
+    int i,j;
 
     // shift memory
 
@@ -102,51 +169,7 @@ void PlotScatter::draw(wxAutoBufferedPaintDC& dc)
 
     for(j=0; i < scatterMemSyms; i++,j++)
     {
-        m_mem[i] = m_new_samples[j];
-    }
-
-    // automatically scale
-
-    float max_xy = 1E-12;
-    float real,imag;
-    for(i=0; i< scatterMemSyms; i++) {
-        real = fabs(m_mem[i].real);
-        imag = fabs(m_mem[i].imag);
-        if (real > max_xy)
-            max_xy = real;
-        if (imag > max_xy)
-            max_xy = imag;
-    }
-    m_filter_max_xy = BETA*m_filter_max_xy + (1 - BETA)*2.5*max_xy;
-    if (m_filter_max_xy < 0.001)
-        m_filter_max_xy = 0.001;
-    //printf("max_xy: %f m_filter_max_xy: %f\n", max_xy, m_filter_max_xy);
-
-    x_scale = (float)m_rGrid.GetWidth()/m_filter_max_xy;
-    y_scale = (float)m_rGrid.GetHeight()/m_filter_max_xy;
-
-    // draw all samples
-
-    for(i = 0; i < scatterMemSyms; i++)
-    {
-        x = x_scale * m_mem[i].real + m_rGrid.GetWidth()/2;
-        y = y_scale * m_mem[i].imag + m_rGrid.GetHeight()/2;
-	x += PLOT_BORDER + XLEFT_OFFSET;
-	y += PLOT_BORDER;
-	dc.DrawPoint(x, y);
-    }
-}
-
-//----------------------------------------------------------------
-// add_new_samples()
-//----------------------------------------------------------------
-void PlotScatter::add_new_samples(COMP samples[])
-{
-    int i;
-
-    for(i = 0; i < Nsym; i++)
-    {
-        m_new_samples[i] = samples[i];
+        m_mem[i] = samples[j];
     }
 }
 
